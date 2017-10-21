7e869f6e1574cfc812f58c4b9dc880ab37e9e7ce
Sebastian Kämpfer <sebastian.kaempfer@bertelsmann.de>
2017-10-21 17:28:53 +0200

    Updated SqlServer provider to netstandard2.0

--- src/providers/WorkflowCore.Persistence.SqlServer/MigrationContextFactory.cs
+++ src/providers/WorkflowCore.Persistence.SqlServer/MigrationContextFactory.cs
@@ -3,12 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace WorkflowCore.Persistence.SqlServer
 {
-    public class MigrationContextFactory : IDbContextFactory<SqlServerPersistenceProvider>
+    public class MigrationContextFactory : IDesignTimeDbContextFactory<SqlServerPersistenceProvider>
     {
-        public SqlServerPersistenceProvider Create(DbContextFactoryOptions options)
+        public SqlServerPersistenceProvider CreateDbContext(string[] args)
         {
             return new SqlServerPersistenceProvider(@"Server=.;Database=WorkflowCore;Trusted_Connection=True;", true, true);
         }
